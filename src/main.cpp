79d643e1887ca10c30093d45e8f4096b2669f075
JPbooya <perezjcarlos81@gmail.com>
2026-01-14 18:43:06 +0000

    fixed functions to show different background colors

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,19 @@
 int main() {
 bn::core::init();
 while(true) {
-  if (bn::keypad::a_pressed()) {
+  if (bn::keypad::a_held()) {
    
     bn::backdrop::set_color(bn::color(10, 10, 22));
   }
   // a_held();
-  if (bn::keypad::b_pressed()) {
+  else if (bn::keypad::b_held()) {
     bn::backdrop::set_color(bn::color(28, 30, 31 ));
   }
 
-  // else {
-  //   bn::backdrop::set_color(bn::color(31, 22, 22));
-  // }
+  else {
+    bn::backdrop::set_color(bn::color(31, 22, 22));
+  }
 
     bn::core::update(); 
-
   }
 } 
