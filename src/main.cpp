bea05bc235c65d6dd231214e6d02cd041d457da7
JPbooya <perezjcarlos81@gmail.com>
2026-01-14 03:01:14 +0000

    added a press function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <bn_backdrop.h>
 #include <bn_color.h>
 #include <bn_core.h>
+#include <bn_keypad.h>
 
 int main() {
-  bn::core::init();
-  bn::backdrop::set_color(bn::color(15, 29, 15));
 
-  while(true) {
-    bn::core::update();
+while(true) {
+  if (bn::keypad::a_pressed()) {
+
+   // bn::core::init();
+    bn::backdrop::set_color(bn::color(15, 29, 15));
   }
-}
+
+    bn::core::update(); 
+
+  }
+} 
